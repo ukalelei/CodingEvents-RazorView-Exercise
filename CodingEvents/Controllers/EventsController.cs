15093e54477f98f5607cb5c35e66d258daa422a6
ukalelei <75921243+ukalelei@users.noreply.github.com>
2021-09-06 23:19:10 -0500

    Store user's input to a Dictionary instead of List. Added description parameter to NewEvent action method. Replace ul to table. Modify _Layout.cshtml to display  coding events table when Home tab is clicked, and added  Add tab that will direct user to page that contains a form to fill out and  submit

--- CodingEvents/Controllers/EventsController.cs
+++ CodingEvents/Controllers/EventsController.cs
@@ -10,22 +10,17 @@ namespace CodingEvents.Controllers
 {
     public class EventsController : Controller
     {
-        //events will be stored in the empty list 
-        static private List<string> Events = new List<string>();
+        //events will be stored dictonary
+        static private Dictionary<string, string> meetingEvents = new Dictionary<string, string>();
 
         // GET: /<controller>/Events
         [HttpGet] //index only respond to get request
         public IActionResult Index()
         {
-            //add events to empty list
-            /* this is not needed anymore since we already a New event action method line 48 that adds event
-             *  Events.Add("Strange Loop");
-                Events.Add("Grace Hopper");
-                Events.Add("Code with Pride");*/ 
 
             //to access events list in a view, events are added to a property name "event" in viewbag
             //viewbag: carries variables from the controller into the view
-            ViewBag.events = Events; //events is added to viewbag
+            ViewBag.events = meetingEvents; //events is added to viewbag
 
             return View();
         }
@@ -34,7 +29,6 @@ namespace CodingEvents.Controllers
         [HttpGet]
         public IActionResult Add()//If the action attribute in the <form> tag leads to the same route as the form is being rendered at, you do not have to include an action attribute.
         {
-
             return View(); //Add.cshtml
         }
 
@@ -42,9 +36,9 @@ namespace CodingEvents.Controllers
         //Route /Events/Add
         [HttpPost]
         [Route("/Events/Add")]
-        public IActionResult NewEvent(string name) //this method handles form submission
+        public IActionResult NewEvent(string name, string description) //this method handles form submission
         {
-            Events.Add(name); //user input get add to event list 
+            meetingEvents.Add(name, description); //user input get add to event list 
             return Redirect("/Events"); //Redirect user to Index.
                                         //Redirect send user to different page after submitting form.
             
