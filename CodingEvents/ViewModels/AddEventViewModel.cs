f0ecccc38c3f6a02890956732fe0a45c42892f3c
ukalelei <75921243+ukalelei@users.noreply.github.com>
2021-09-20 22:11:24 -0500

    add validation attributes for EventLocation and NumberOfAttendees

--- CodingEvents/ViewModels/AddEventViewModel.cs
+++ CodingEvents/ViewModels/AddEventViewModel.cs
@@ -14,8 +14,19 @@ namespace CodingEvents.ViewModels
         [StringLength(500, ErrorMessage = "Description needs to be less than 500 characters")] //require length of string field 
         public string Description { get; set; }
 
-
         [EmailAddress] //make sure user enter email
         public string ContactEmail { get; set; }
+
+        //cannot be null or blank
+        [Required(ErrorMessage = "Event location require")]
+        public string EventLocation { get; set; }
+
+        //valid num 0-100000
+        [Required(ErrorMessage ="Number of attendees require")]
+        [RegularExpression("([1-9][0-9]*)", ErrorMessage = "Input can only contain characters 0-9")]
+        [Range(0, 100000, ErrorMessage = "Cannot exeed 100000 attendees")]
+        public string NumberOfAttendees { get; set; }
+
+        
     }
 }
