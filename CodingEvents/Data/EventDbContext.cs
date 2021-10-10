02056c764cabcc83c0964c57e49d7b29992ecd36
ukalelei <75921243+ukalelei@users.noreply.github.com>
2021-10-10 03:56:12 -0500

    create EventCategory class for storing categories. Migrate and update to database

--- CodingEvents/Data/EventDbContext.cs
+++ CodingEvents/Data/EventDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using CodingEvents.Models;
 using CodingEventsDemo.Models;
 using Microsoft.EntityFrameworkCore;
 
@@ -7,6 +8,7 @@ namespace CodingEvents.Data
     public class EventDbContext : DbContext
     {
         public DbSet<Event> Events { get; set; }
+        public DbSet<EventCategory> EventCategories { get; set; }
 
         public EventDbContext(DbContextOptions<EventDbContext> options)
               : base(options)
---
+++ CodingEvents/Models/EventCategory.cs
@@ -0,0 +1,37 @@
+﻿using System;
+namespace CodingEvents.Models
+{
+    //represents data that will be stored in our database
+    public class EventCategory
+    {
+        public int Id { get; set; } //Primary key properties must have both a getter and setter
+        public string Name { get; set; }
+
+        public EventCategory() //no agument constructor
+        {
+        }
+
+        public EventCategory(string name)
+        {
+            Name = name;
+        }
+
+        public override string ToString()
+        {
+            return Name;
+
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is EventCategory @eventCategory &&
+
+                Id == @eventCategory.Id;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Id);
+        }
+    }
+}
