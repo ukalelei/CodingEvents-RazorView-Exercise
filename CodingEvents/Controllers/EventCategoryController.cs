554b3ea3296b6efa22c2020cf819e718fc9c55d1
ukalelei <75921243+ukalelei@users.noreply.github.com>
2021-10-10 03:59:15 -0500

    Create new controller class responding and to populate a new view in EventCategory directory

---
+++ CodingEvents/Controllers/EventCategoryController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using CodingEvents.Data;
+using CodingEvents.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodingEvents.Controllers
+{
+    public class EventCategoryController : Controller
+    {
+        private EventDbContext context;
+        public EventCategoryController(EventDbContext dbContext)
+        {
+            context = dbContext;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            //Pass DbContext category values as a list into the view template as a model
+            List<EventCategory> categories = context.EventCategories.ToList();
+            return View(categories);
+        }
+
+       
+
+    }
+}
