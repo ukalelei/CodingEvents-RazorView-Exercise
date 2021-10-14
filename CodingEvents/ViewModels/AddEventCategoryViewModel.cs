52ff4aa89d6c396af10cc0c8dccd55dee4e4e090
ukalelei <75921243+ukalelei@users.noreply.github.com>
2021-10-13 19:45:35 -0500

    Create AddEventCategory view model with Name property and validation attributes

---
+++ CodingEvents/ViewModels/AddEventCategoryViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace CodingEvents.ViewModels
+{
+    public class AddEventCategoryViewModel
+    {
+        [Required]
+        [StringLength(20, MinimumLength = 3, ErrorMessage = "must be 3 to 20 characters long")]
+        public string Name { get; set; }
+    }
+}
