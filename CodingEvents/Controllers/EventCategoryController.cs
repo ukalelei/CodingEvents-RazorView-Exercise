9791e176eb2ddad30168b804f49eb178d0e9e6fb
ukalelei <75921243+ukalelei@users.noreply.github.com>
2021-10-13 19:47:27 -0500

    Create two action method: Create(), ProcessCreateEventCategoryForm()

--- CodingEvents/Controllers/EventCategoryController.cs
+++ CodingEvents/Controllers/EventCategoryController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using CodingEvents.Data;
 using CodingEvents.Models;
+using CodingEvents.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CodingEvents.Controllers
@@ -23,7 +24,33 @@ namespace CodingEvents.Controllers
             return View(categories);
         }
 
-       
+        [HttpGet]
+        public IActionResult Create()
+        {
+            //Pass a new instance
+            AddEventCategoryViewModel addEventCategoryViewModel = new AddEventCategoryViewModel();
+            return View(addEventCategoryViewModel);
+        }
+
+        [HttpPost]
+        [Route("/EventCategory/Create")]
+        public IActionResult ProcessCreateEventCategoryForm(AddEventCategoryViewModel addEventCategoryViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                EventCategory newCategory = new EventCategory
+                {
+                    Name = addEventCategoryViewModel.Name
+                };
+
+                context.EventCategories.Add(newCategory); //add to database if form input meets the validation conditions
+                context.SaveChanges(); //save changes to database
+                return Redirect("/EventCategory/Index");
+            }
+            return View("Create");
+        }
+
+
 
     }
 }
