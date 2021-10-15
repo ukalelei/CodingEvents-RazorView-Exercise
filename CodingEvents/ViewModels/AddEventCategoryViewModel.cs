1e72aba7cd524c668368cf006746d71ff7430c84
ukalelei <75921243+ukalelei@users.noreply.github.com>
2021-10-15 00:39:03 -0500

    create new property with validation attribute

--- CodingEvents/ViewModels/AddEventCategoryViewModel.cs
+++ CodingEvents/ViewModels/AddEventCategoryViewModel.cs
@@ -1,12 +1,17 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using CodingEvents.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace CodingEvents.ViewModels
 {
     public class AddEventCategoryViewModel
     {
-        [Required]
+        [Required(ErrorMessage = "Name is required")]
         [StringLength(20, MinimumLength = 3, ErrorMessage = "must be 3 to 20 characters long")]
         public string Name { get; set; }
+
     }
+
 }
