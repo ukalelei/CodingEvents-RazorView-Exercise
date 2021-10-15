ba106a58e4b39379122d169edb88534f221eb3db
ukalelei <75921243+ukalelei@users.noreply.github.com>
2021-10-15 00:42:14 -0500

    Update property name

--- CodingEvents/Data/EventDbContext.cs
+++ CodingEvents/Data/EventDbContext.cs
@@ -7,8 +7,8 @@ namespace CodingEvents.Data
 {
     public class EventDbContext : DbContext
     {
-        public DbSet<Event> Events { get; set; }
-        public DbSet<EventCategory> EventCategories { get; set; }
+        public DbSet<Event> Events { get; set; } //primary key
+        public DbSet<EventCategory> Categories { get; set; }
 
         public EventDbContext(DbContextOptions<EventDbContext> options)
               : base(options)
