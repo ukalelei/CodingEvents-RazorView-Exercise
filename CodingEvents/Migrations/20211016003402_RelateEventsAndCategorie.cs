9ee35b3f36bc7d78c3d215f5737faddc558dc7ca
ukalelei <75921243+ukalelei@users.noreply.github.com>
2021-10-15 19:47:00 -0500

    Create db migration

---
+++ CodingEvents/Migrations/20211016003402_RelateEventsAndCategorie.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CodingEvents.Migrations
+{
+    public partial class RelateEventsAndCategorie : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_EventCategories",
+                table: "EventCategories");
+
+            migrationBuilder.RenameTable(
+                name: "EventCategories",
+                newName: "Categories");
+
+            migrationBuilder.RenameColumn(
+                name: "Type",
+                table: "Events",
+                newName: "CategoryId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Categories",
+                table: "Categories",
+                column: "Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Events_CategoryId",
+                table: "Events",
+                column: "CategoryId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Events_Categories_CategoryId",
+                table: "Events",
+                column: "CategoryId",
+                principalTable: "Categories",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Events_Categories_CategoryId",
+                table: "Events");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Events_CategoryId",
+                table: "Events");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Categories",
+                table: "Categories");
+
+            migrationBuilder.RenameTable(
+                name: "Categories",
+                newName: "EventCategories");
+
+            migrationBuilder.RenameColumn(
+                name: "CategoryId",
+                table: "Events",
+                newName: "Type");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_EventCategories",
+                table: "EventCategories",
+                column: "Id");
+        }
+    }
+}
