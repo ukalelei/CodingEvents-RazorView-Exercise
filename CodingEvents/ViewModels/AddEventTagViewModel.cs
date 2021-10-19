78b367c3c93366e20e62881be299939ac845a93d
ukalelei <75921243+ukalelei@users.noreply.github.com>
2021-10-19 12:50:54 -0500

    add viewmodel to render and process our tag form

---
+++ CodingEvents/ViewModels/AddEventTagViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using CodingEvents.Models;
+using CodingEventsDemo.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace CodingEvents.ViewModels
+{
+    public class AddEventTagViewModel //allow the user to select the tag to add to the event with ID X
+    {
+        [Required(ErrorMessage = "Event is required")]
+        public int EventId { get; set; }
+
+        [Required(ErrorMessage = "Tag is required")]
+        public int TagId { get; set; }
+
+        public Event Event { get; set; }
+
+        public List<SelectListItem> Tags { get; set; }
+
+        public AddEventTagViewModel(Event theEvent, List<Tag> possibleTags)
+        {
+            Tags = new List<SelectListItem>();
+
+            foreach (var tag in possibleTags) //loops thru collection of possible tags
+            {
+                Tags.Add(new SelectListItem //for each loop create corresponding selectlistitem that is used to render drop down option
+                {
+                    Value = tag.Id.ToString(),
+                    Text = tag.Name
+                });
+            }
+
+            Event = theEvent;
+        }
+
+        public AddEventTagViewModel()
+        {
+        }
+    }
+}
